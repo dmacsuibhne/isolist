0c455c508b16b1a5bda043b3979224c33ceb29f2
Donnchadh Mac Suibhe <donnchadh.macsuibhne@ammeon.com>
2017-01-05 18:07:30 +0000

    finished for thursday

--- isolist.py
+++ isolist.py
@@ -9,44 +9,49 @@ def isolist():
     app_directory=os.path.dirname(os.path.realpath(__file__))
     data_directory="".join([app_directory,"data/"])
 
-    xml_filepaths = get_filepaths(app_directory +"/data")
+    xml_filepath_list = get_filepaths(app_directory +"/data")
 
-    (tree_dict, root_dict) = parse_xml(xml_filepaths)
+    # (tree_dict, root_dict) = parse_xml(xml_filepath_list)
 
-    # table=[]
-    # table.append({'row1':xml_filepaths, 'row2':'lo',})
+    # iso_tree = ET.parse('/home/donnchadh.macsuibhne/Desktop/isolist/isolist/data/ERIClitp_CXP9024296-2.54.3.iso.xml')
+    # iso_root = iso_tree.getroot()
 
+    iso_tree_list, iso_root_list = parse_xml(xml_filepath_list)
 
-
-    return render_template('isolist.html', root_dict=root_dict)
+    # return render_template('isolist.html', root_dict=root_dict)
+    return render_template('isolist.html', iso_list=iso_root_list)
 
 def get_filepaths(directory):
     """
     This function will generate the file names in a directory 
     tree by walking the tree either top-down or bottom-up.
     """
-    file_paths = []  # List which will store all of the full filepaths.
+    file_path_list = []  # List which will store all of the full filepaths.
 
     # Walk the tree.
     for root, directories, files in os.walk(directory):
         for filename in files:
             # Join the two strings in order to form the full filepath.
             filepath = os.path.join(root, filename)
-            file_paths.append(filepath)  # Add it to the list.
+            file_path_list.append(filepath)  # Add it to the list.
 
-    return file_paths  # Self-explanatory.
+    return file_path_list  # Self-explanatory.
 
-def parse_xml(xml_filepaths):
+def parse_xml(xml_filepath_list):
     """
     Parses all xml files in data folder
     """
-    tree_dict = {}
-    root_dict = {}
-    for file in xml_filepaths:
+    tree_list = []
+    root_list = []
+    for file in xml_filepath_list:
         tree = ET.parse(file)
-        tree_dict[file] = tree
+        tree_list.append(tree)
 
         root = tree.getroot()
-        root_dict[file] = root
+        root_list.append(root)
 
-    return (tree_dict, root_dict)
+    #
+    # app.logger.debug('DEBUG TEXT')
+    # app.logger.debug(root_dict)
+    #
+    return (tree_list, root_list)
